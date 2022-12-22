494dae484339af04be7fef2e0da8bdb60abae591
Willennn <109761991+Willennn@users.noreply.github.com>
2022-12-22 15:11:31 +0100

    Update form.py

--- form.py
+++ form.py
@@ -39,6 +39,15 @@ def regle_jeu():
     print()
 
     print(55 * " ", end="")
-    print("blabla")
+    print("Vous devrez choisir une dimension XY pour le plateau désiré 
+    print(55 * " ", end="")          
+    print("Vous aurez le choix entre un triangle, un losange et un cercle")
     print(55 * " ", end="")
-    print("blablabla")
+    print("Vous devrez ensuite choisir entre 2 modes de jeu :")
+    print(55 * " ", end="")
+    print("Le mode facile vous permet de choisir de placer n'importe quel bloc possible pour le plateau choisi")      
+    print(55 * " ", end="")
+    print("Le mode difficile vous restreint à 3 blocs aléatoire parmi les blocs possibles pour ce même plateau")
+    print(55 * " ", end="")
+    print("Ce mode vous permettera néanmoins d'obtenir plus de points pour chaque ligne ou colonne complétée")
+    
