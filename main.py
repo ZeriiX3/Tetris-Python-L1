18e42aeb38f49435db7a400fd810bbe1bdab91ef
Willennn <109761991+Willennn@users.noreply.github.com>
2022-12-25 14:18:43 +0100

    Update main.py

--- main.py
+++ main.py
@@ -35,10 +35,12 @@ while run_game:
     init_screen(screen, choix_mode)
 
     vies = 3
+    score = 0
     while vies > 0:
+        print()
         print_grid(mat)                                    # Affiche le plateau
         affiche_tout(screen, choix_plateau, choix_mode)    # Affiche les blocs possibles à placer
-        indice = select_bloc(choix_plateau)            # Selectionne le bloc choisis par l'utilisateur
+        indice = select_bloc(choix_plateau)                # Selectionne le bloc choisis par l'utilisateur
         if choix_mode == 2:
             while indice > 2:
                 print("Ce bloc n'est pas parmi ceux proposés aléatoirement")
@@ -49,16 +51,28 @@ while run_game:
         if place_bloc(mat, x, y,indice) is False:          # Vérifie et place le bloc si possible, cas contraire -> perte d'une vie
             vies -= 1
             print("Nombre de vies restantes : ", vies)
+        else :
+            print("Le bloc ",indice," a bien été déposé aux coordonnées x :", minuscule[x]," y :",majuscule[y])
+            print()
         for i in range(len(mat)):
             if row_state(mat, i) is True:
                 print_grid(mat)
+                if choix_mode == 1 :
+                    score = score + mat[i].count(2)
+                if choix_mode == 2 :
+                    score = score + (mat[i].count(2) * 2)
+                print("Vous avez rempli une ligne !")
                 row_clear(mat, i)
             if col_state(mat, i) is True:
                 print_grid(mat)
-                col_clear(mat, i)
+                if choix_mode == 1 :
+                    score = score + col_clear(mat,i)
+                if choix_mode == 2 :
+                    score = score + (col_clear(mat,i)*2)
+                print("Vous avez rempli une colonne !")
                 #mettre un clear ici bg
-        print("Votre score est de : ",score)
-    print("Vous avez perdu, votre score final est de : ",score)
+        print("Votre score est de : ", score)
+    print("Vous avez perdu, votre score final est de : ", score)
     save_grid(mat) # Stock le plateau dans le ficher PLATEAU.TXT
 
     run_game = False
